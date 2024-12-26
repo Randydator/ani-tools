20c880be9c4ea2f100e06c27fe071ee913443d6e
Randydator <nils.markewitz@gmail.com>
2024-12-26 13:40:54 +0100

    adjust some code comment

--- src/components/Header/authenticate.ts
+++ src/components/Header/authenticate.ts
@@ -2,12 +2,11 @@
 
     Cookies can be made secure enough with the secure flag and the same-site flag.
     This prevents against man in the middle interceptions and cross-site scripting attacks.
-    It is only vulnerable to CSRF attacks. 
+    It is only vulnerable to XSS attacks. 
     To counteract this, I need to be very careful to sanatize all user input and output. 
     This should make browser cookies safe enough to use.
 
-    If I need more, I could use my set up cloudflare d1 and implment some sort of session management system
-    with encryption that can also shorten the tokens. 
+    If I need more, I could use my set up cloudflare d1 and implment some sort of session management system. 
 */
 
 export function login() {
